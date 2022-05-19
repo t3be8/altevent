794818400e5c4a9a08815aa7845ea7842e7b2b20
alkaprasetya <66933824+alkaprasetya@users.noreply.github.com>
2022-05-19 10:09:15 +0700

    update event part 3

--- delivery/views/req/events.go
+++ delivery/views/req/events.go
@@ -1,29 +1,27 @@
 package req
 
-import "time"
-
 type CreateEventRequest struct {
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Rules       string    `json:"rules"`
-	Organizer   string    `json:"organizer"`
-	DueDate     float64   `json:"duedate"`
-	BeginAt     time.Time `json:"begin"`
-	Location    string    `json:"location"`
-	Ticket      int       `json:"ticket"`
-	Links       string    `json:"links"`
-	Banner      string    `json:"banner"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Rules       string `json:"rules"`
+	Organizer   string `json:"organizer"`
+	DueDate     string `json:"duedate"`
+	BeginAt     string `json:"begin"`
+	Location    string `json:"location"`
+	Ticket      int    `json:"ticket"`
+	Links       string `json:"links"`
+	Banner      string `json:"banner"`
 }
 
 type UpdateEventRequest struct {
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Rules       string    `json:"rules"`
-	Organizer   string    `json:"organizer"`
-	DueDate     float64   `json:"duedate"`
-	BeginAt     time.Time `json:"begin"`
-	Location    string    `json:"location"`
-	Ticket      int       `json:"ticket"`
-	Links       string    `json:"links"`
-	Banner      string    `json:"banner"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Rules       string `json:"rules"`
+	Organizer   string `json:"organizer"`
+	DueDate     string `json:"duedate"`
+	BeginAt     string `json:"begin"`
+	Location    string `json:"location"`
+	Ticket      int    `json:"ticket"`
+	Links       string `json:"links"`
+	Banner      string `json:"banner"`
 }
