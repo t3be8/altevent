7b1adce2b5c7895d4002bb5be31756b7e58f320c
alkaprasetya <66933824+alkaprasetya@users.noreply.github.com>
2022-05-19 10:24:19 +0700

    update route dan main

--- delivery/routes/route.go
+++ delivery/routes/route.go
@@ -1,13 +1,14 @@
 package routes
 
 import (
+	event "altevent/delivery/controllers/events"
 	"altevent/delivery/controllers/user"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func RegisterPath(e *echo.Echo, uc user.IUserController) {
+func RegisterPath(e *echo.Echo, uc user.IUserController, ec event.IEventController) {
 	e.Pre(middleware.RemoveTrailingSlash())
 
 	e.Use(middleware.CORS())
@@ -21,7 +22,17 @@ func RegisterPath(e *echo.Echo, uc user.IUserController) {
 	// Users route
 	apiGroup.GET("/users/:id", uc.Show(), middleware.JWTWithConfig(middleware.JWTConfig{SigningKey: []byte("ALTEVEN")}))
 	apiGroup.PUT("/users/:id", uc.Update(), middleware.JWTWithConfig(middleware.JWTConfig{SigningKey: []byte("ALTEVEN")}))
-	apiGroup.PUT("/users/:id", uc.Delete(), middleware.JWTWithConfig(middleware.JWTConfig{SigningKey: []byte("ALTEVEN")}))
+	apiGroup.DELETE("/users/:id", uc.Delete(), middleware.JWTWithConfig(middleware.JWTConfig{SigningKey: []byte("ALTEVEN")}))
+
+	// Events route
+	apiGroup.POST("/events", ec.InsertEvent())
+	// apiGroup.POST("/events/:id/join", ec.InsertEvent())
+	apiGroup.GET("/events", ec.SelectEvent())
+	// apiGroup.GET("/events?title", ec.SelectEvent())
+	// apiGroup.GET("/events?byme", ec.SelectEvent())
+	apiGroup.GET("/events/:id", ec.GetEventById(), middleware.JWTWithConfig(middleware.JWTConfig{SigningKey: []byte("ALTEVEN")}))
+	apiGroup.PUT("/events/:id", ec.UpdateEvent(), middleware.JWTWithConfig(middleware.JWTConfig{SigningKey: []byte("ALTEVEN")}))
+	apiGroup.DELETE("/events/:id", ec.DeleteEvent(), middleware.JWTWithConfig(middleware.JWTConfig{SigningKey: []byte("ALTEVEN")}))
 
 	// Order ticket route
 	// apiGroup.POST("/orders", oc.CreateOrder(), middleware.JWTWithConfig(middleware.JWTConfig{SigningKey: []byte("RU$SI4")}))
