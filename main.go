7ab1e094b1fcce004cdaeb4c2ce8cd372c655323
adeeplearn <galang8280@students.amikom.ac.id>
2022-05-20 07:05:15 +0700

    update main.go

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"altevent/config"
+	commentController "altevent/delivery/controllers/comment"
 	eventController "altevent/delivery/controllers/events"
 	userController "altevent/delivery/controllers/user"
 	"altevent/delivery/routes"
+	CommentRepo "altevent/repository/comment"
 	eventRepo "altevent/repository/events"
 	userRepo "altevent/repository/user"
 	"fmt"
@@ -22,12 +24,13 @@ func main() {
 	e := echo.New()
 
 	repoUser := userRepo.New(db)
-
 	repoEvents := eventRepo.New(db)
+	CommentRepo := CommentRepo.New(db)
 
 	controllerUser := userController.New(repoUser, validator.New())
 	controllerEvent := eventController.New(repoEvents, validator.New())
+	controllerComment := commentController.New(CommentRepo, validator.New())
 
-	routes.RegisterPath(e, controllerUser, controllerEvent)
+	routes.RegisterPath(e, controllerUser, controllerEvent, controllerComment)
 	log.Fatal(e.Start(fmt.Sprintf(":%d", conf.Port)))
 }
